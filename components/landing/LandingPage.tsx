bedca54dee839503684f9f54b59c3c5469c47767
Ola <112015383+olasores@users.noreply.github.com>
2026-02-23 10:48:42 -0800

    removed status bar

--- components/landing/LandingPage.tsx
+++ components/landing/LandingPage.tsx
@@ -24,7 +24,7 @@ export function LandingPage() {
 
       <Navigation />
       <Hero />
-      <StatsBar />
+      {/* <StatsBar /> */}
       <HowItWorks />
       <Categories />
       <Testimonials />
